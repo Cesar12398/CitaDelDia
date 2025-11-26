4081bc3c5578bf4fdbb1a6e7a9508f839649a378
tardes <merino12398@gmail.com>
2025-11-26 20:19:06 +0100

    Ejercicio cita

--- app/src/main/java/com/cesarm/citadeldia/MainActivity.kt
+++ app/src/main/java/com/cesarm/citadeldia/MainActivity.kt
@@ -12,7 +12,15 @@ class MainActivity : AppCompatActivity() {
     lateinit var tvCita : TextView
     lateinit var tvAutor : TextView
     lateinit var  btnNuevaCita : Button
+    private val listaCitas = listOf(
+        Cita ("La vida es sueño", "Calderón de la Barca"),
+        Cita ( "Pienso, luego existo", "René Descartes"),
+        Cita ("El conocimiento es poder", "Francis Bacon"),
+        Cita ("No hay caminos para la paz; la paz es el camino", "Mahatma Gandhi"),
+        Cita("La imaginación es más importante que el conocimiento", "Albert Einstein"),
+        Cita("No se puede encontrar la paz evitando la vida", "Virginia Woolf")
 
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -22,17 +30,17 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        private val listaCitas = listOf(
-            Cita ("La vida es sueño", "Calderón de la Barca"),
-            Cita ( "Pienso, luego existo", "René Descartes"),
-            Cita ("El conocimiento es poder", "Francis Bacon")
-        )
 
-        tvCita.findViewById(R.id.tv_cita)
-        tvAutor.findViewById(R.id.tv_autor)
-        btnNuevaCita.findViewById(R.id.btn_nueva_cita)
+        tvCita=findViewById(R.id.tv_cita)
+        tvAutor=findViewById(R.id.tv_autor)
+        btnNuevaCita=findViewById(R.id.btn_nueva_cita)
 
         //Acción del boton
+        btnNuevaCita.setOnClickListener {
+            val citaAleatoria = listaCitas.random()
+            tvCita.text = "\"${citaAleatoria.texto}\""
+            tvAutor.text = "-${citaAleatoria.autor}"
+        }
 
 
 
